055e99f89efe246a6ba8bf511d9e9ff7a66402db
João Lucas de Oliveira Lopes <jlucaso@hotmail.com>
2021-11-30 13:51:12 -0300

    Refactored with best practices

--- lib/summoner/summoner.dart
+++ lib/summoner/summoner.dart
@@ -1,27 +1,43 @@
 import 'package:lcu_connector/lcu.dart';
 
 class Summoner {
-  int? accountId;
-  String? displayName;
-  String? internalName;
-  bool? nameChangeFlag;
-  String? puuid;
-  int? profileIconId;
-  int? summonerId;
-  int? summonerLevel;
-  int? xpSinceLastLevel;
-  int? xpUntilNextLevel;
-  Summoner.fromJson(Map<String, dynamic> json) {
-    accountId = json["accountId"];
-    displayName = json["displayName"];
-    internalName = json["internalName"];
-    nameChangeFlag = json["nameChangeFlag"];
-    puuid = json["puuid"];
-    profileIconId = json["profileIconId"];
-    summonerId = json["summonerId"];
-    summonerLevel = json["summonerLevel"];
-    xpSinceLastLevel = json["xpSinceLastLevel"];
-    xpUntilNextLevel = json["xpUntilNextLevel"];
+  final int accountId;
+  final String displayName;
+  final String internalName;
+  final bool nameChangeFlag;
+  final String puuid;
+  final int profileIconId;
+  final int summonerId;
+  final int summonerLevel;
+  final int xpSinceLastLevel;
+  final int xpUntilNextLevel;
+
+  Summoner({
+    required this.accountId,
+    required this.displayName,
+    required this.internalName,
+    required this.nameChangeFlag,
+    required this.puuid,
+    required this.profileIconId,
+    required this.summonerId,
+    required this.summonerLevel,
+    required this.xpSinceLastLevel,
+    required this.xpUntilNextLevel,
+  });
+
+  factory Summoner.fromJson(Map<String, dynamic> json) {
+    return Summoner(
+      accountId: json['accountId'],
+      displayName: json['displayName'],
+      internalName: json['internalName'],
+      nameChangeFlag: json['nameChangeFlag'],
+      puuid: json['puuid'],
+      profileIconId: json['profileIconId'],
+      summonerId: json['summonerId'],
+      summonerLevel: json['summonerLevel'],
+      xpSinceLastLevel: json['xpSinceLastLevel'],
+      xpUntilNextLevel: json['xpUntilNextLevel'],
+    );
   }
 }
 
