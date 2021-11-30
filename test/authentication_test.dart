ab91486fb8a3d63a32a5caf70ea1c8310a84ec15
João Lucas de Oliveira Lopes <jlucaso@hotmail.com>
2021-11-30 13:50:47 -0300

    Improve test method

--- test/authentication_test.dart
+++ test/authentication_test.dart
@@ -2,9 +2,21 @@ import 'package:lcu_connector/authentication.dart';
 import 'package:test/test.dart';
 
 void main() async {
-  test('try authenticate', () async {
+  test('try authenticate with client open', () async {
     var authentication = Authentication();
-    var result = await authentication.CheckLolRunning();
-    expect(result, true);
+    var tryCount = 0;
+    await Future.doWhile(() async {
+      var result = await authentication.CheckLolRunning();
+      if (!result) {
+        print("Lol client not running... tryCount: $tryCount");
+        await Future.delayed(Duration(seconds: 2));
+        tryCount++;
+        if (tryCount >= 5) {
+          throw Exception("Lol client not running");
+        }
+      }
+      return !result;
+    });
+    print('done');
   });
 }
